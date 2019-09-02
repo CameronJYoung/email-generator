1f8862fee29d6b30be0fd35d99696f006b67ef09
Cameron Young <cameronjyoung123@gmail.com>
2019-09-02 12:59:22 +0100

    folder structure and added some email templates

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const panini = require('panini');
+const sass = require('gulp-sass');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+
+let convertHbsTask = (done) => {
+	panini.refresh();
+	gulp.src('./src/templates/pages/*.html')
+		.pipe(panini ({
+			root: './src/templates/pages/',
+			layouts: './src/templates/layouts/',
+			partials: './src/templates/partials/',
+			data: './src/data/',
+//			helpers: 'helpers/',
+		}))
+		.pipe(gulp.dest('dist/pages'));
+	done();
+}
