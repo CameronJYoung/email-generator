9136597d231d4650cfde7885f8a2eb981ee0ccda
Cameron Young <cameronjyoung123@gmail.com>
2019-09-12 12:51:44 +0100

    added juice and almost working code. Also added scss and browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,93 @@ const panini = require('panini');
 const sass = require('gulp-sass');
 const del = require('del');
 const browserSync = require('browser-sync').create();
+const inky = require('inky');
+const fs = require('fs');
+const juice = require('juice');
 
-let convertHbsTask = (done) => {
+//Tasks
+let convertPaniniTask = (done) => {
 	panini.refresh();
 	gulp.src('./src/templates/pages/*.html')
 		.pipe(panini ({
 			root: './src/templates/pages/',
 			layouts: './src/templates/layout/',
 			partials: './src/templates/partials/',
-			data: './src/data/',
-//			helpers: 'helpers/',
+			data: './src/assets/data',
+			helpers: './src/helpers/',
 		}))
-		.pipe(gulp.dest('dist/pages'));
+		.pipe(inky({
+			dest: 'dist/pages'
+		}));
+	done();
+}
+
+let convertScssTask = (done) => {
+	gulp.src('./src/assets/css/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./dist/css'));
+	done();
+}
+
+let moveImagesTask = (done) => {
+	gulp.src('./src/assets/img/*.*')
+		.pipe(gulp.dest('./dist/img'));
+	done();
+}
+
+let movePDFTask = (done) => {
+	gulp.src('./src/assets/img/*.pdf')
+		.pipe(gulp.dest('./dist/pdf'));
+	done();
+}
+
+let juicify = (done) => {
+	juice.juiceFile('./dist/pages/example-1.html', {}, (err, html) => {
+		console.log(err);
+		fs.writeFile("./dist/pages/example-1.html", html, (err) => {
+			if(err) {
+				return console.log(err);
+			}
+			console.log("The file was saved!");
+		});
+	});
+	done();
+};
+
+let clearDist = (done) => {
+	del.sync('./dist');
+	done();
+}
+
+let browserSyncReloadTask = (done) => {
+	browserSync.reload();
+	done();
+}
+
+let browserSyncTask = (done) => {
+	browserSync.init({
+		server: {
+			baseDir: './dist',
+			index: 'pages/example-1.html',
+			notify: false
+		}
+	});
+	done();
+}
+
+let watchTask = (done) => {
+	gulp.watch('./src').on('change', gulp.series(clearDist,convertPaniniTask,convertScssTask,moveImagesTask,browserSyncReloadTask));
 	done();
 }
 
 
-exports.hbs = convertHbsTask;
+//Dev Task
+exports.dev = gulp.series(clearDist,gulp.parallel(convertPaniniTask,convertScssTask,moveImagesTask),browserSyncTask,watchTask);
+
+//Build Task
+exports.juice = juicify;
+exports.test = gulp.series(gulp.parallel(convertPaniniTask,convertScssTask));
+exports.build = gulp.series(gulp.parallel(convertPaniniTask,convertScssTask),juicify,moveImagesTask);
+
+//Utility Tasks
+exports.clean = clearDist;
